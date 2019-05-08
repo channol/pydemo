a0ba22392d23157f77a278362899a16cdb9de807
pc14 <424261385@qq.com>
2019-05-08 13:27:04 +0800

    ik

--- snmp.py
+++ snmp.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python3
 import netsnmp
-print("ok")
+import time
+
+sess=netsnmp.Session(Version=2,
+                    DestHost='172.0.20.16',
+                    Community='public')
+
+
+print(1)
+#sess.UseSprintValue=1
+
+print(2)
+#henbgw=netsnmp.Varbind('sysUptime',0)
+vars = netsnmp.VarList(netsnmp.Varbind('sysUpTime', 0),
+                       netsnmp.Varbind('CASA-NFV-HA-MIB::casaHaHeartBeatMsgRecv', 0),
+                       netsnmp.Varbind('CASA-NFV-HA-MIB::casaHaHeartBeatMsgSent', 0))
+                       
+vals=sess.get(vars)
+
+print(vals)
